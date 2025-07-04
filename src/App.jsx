a7c059d54ec745625bb703eb2e614b25c1381085
Kamranhanif012 <mkamranhanif012@gmail.com>
2025-07-04 05:37:50 +0500

    save random color changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Randomcolor from './Components/Random-color/index.jsx'
 import StarRating from './Components/Star-rating/index.jsx'
 import ImageSlider from './Components/Image-slider/index.jsx'
 import LoadMoreData from './Components/load-more-data/index.jsx'
@@ -11,13 +12,13 @@ function App() {
 
   return (
     <>
-    <Randomcolor/>
+    <Randomcolor />
     <StarRating 
      noOfStars={8}  />
 
     <ImageSlider
       url={'https://picsum.photos/v2/list'} page={"1"} limit={"10"}
-    />*/
+    />
 
     <LoadMoreData
 
